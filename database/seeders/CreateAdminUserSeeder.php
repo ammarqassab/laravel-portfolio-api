b9bc1fd23c5fa47e65788bc1acb710d03763351d
AhmadAlaa-99 <https://github.com/AhmadAlaa-99>
2022-07-09 15:01:24 +0200

    finaal

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -14,16 +14,24 @@ class CreateAdminUserSeeder extends Seeder
 */
 public function run()
 {
-    $user = User::create([
-'username'=> 'ahmad99',
+$user = User::create([
+'username'=> 'ahmad',
 'role_as'=> '1',
-'phone'=>'093454345345',
+'phone'=>'093454345343',
 'country'=>'syria',
 'city'=>'damas',
-'email'=> 'ahmad@gmail.com',
-'password'=> bcrypt('123456'),
-'c_password'=> bcrypt('123456'),
+'email'=> 'ahmad99@gmail.com',
+'password'=> bcrypt('32145678'),
+'c_password'=> bcrypt('32145678'), ]);
 
-           ]);
+$user = User::create([
+    'username'=> 'ammar',
+    'role_as'=> '1',
+    'phone'=>'093454345346',
+    'country'=>'syria',
+    'city'=>'damas',
+    'email'=> 'ammar99@gmail.com',
+    'password'=> bcrypt('32145678'),
+    'c_password'=> bcrypt('32145678'), ]);
 }
 }
